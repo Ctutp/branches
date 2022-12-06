bd1ab1fcc8a98c682a988150859b8436efdc17ee
grk_rise <grk.rise@gmail.com>
2022-12-06 22:06:46 +0300

    add times

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
+        System.out.print("Это тоже добавлено в ветке main");
+
     }
     public int add(int a, int b){
         return a + b;
