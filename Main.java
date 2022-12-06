6094fc47e12ab8687073950d63d2673e1e320fe3
grk_rise <grk.rise@gmail.com>
2022-12-06 21:55:49 +0300

    add add

--- Main.java
+++ Main.java
@@ -2,4 +2,7 @@ class Main{
     public static void main(String[] args) {
         System.out.println("Измененов в ветке dev");
     }
+    public int add(int a, int b){
+        return b + a;
+    }
 }
